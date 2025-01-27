f90d2078cec7975b36b0bfe93a53cdb84019c1cb
proger0014 <iakov56@yandex.ru>
2025-01-27 21:06:07 +0600

    updated assets and view

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -16,6 +16,7 @@ class AppAsset extends AssetBundle
     public $css = [
     ];
     public $js = [
+        'js/main.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- views/main/index.php
+++ views/main/index.php
@@ -4,3 +4,12 @@
 
 ?>
 
+<div id="root"></div>
+
+<?= \yii\bootstrap5\ButtonGroup::widget([
+    'buttons' => [
+        ['label' => 'btn 1', 'options' => ['id' => 'btn1', 'class' => 'btn-secondary']],
+        ['label' => 'btn 2', 'options' => ['id' => 'btn2', 'class' => 'btn-primary']],
+        ['label' => 'btn 3', 'options' => ['id' => 'btn3', 'class' => 'btn-secondary']],
+    ]
+]) ?>
